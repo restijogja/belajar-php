e14aa166336cef793777bc43a5f1f094688c12c4
DESKTOP-7696NKA\User <restirohmawati250@gmail.com>
2022-10-28 14:02:34 +0700

    phpdasar1

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama ="Resti";
+$umur =17;
+
+echo "Nama Saya <strong>$nama, saya berusia $umur tahun.</br>";
+
+$namaAdik ="Ovinda";
+$umurAdik ="8";
+
+echo "Nama  adik saya <strong>$namaAdik</strong>, adik saya berusia
+$umurAdik tahun.<br>";
+
+$selisihumur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya(tapi) adalah $selisihumur tahun.";
+
+?>
